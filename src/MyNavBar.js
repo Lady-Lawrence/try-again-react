d807ffb0806cf9eb861a970a3a49a03acaa8c9c7
lady-lawrence <lawrence_ej_carty@post.harvard.edu>
2021-08-16 18:05:46 -0400

    initial commit

---
+++ src/MyNavBar.js
@@ -0,0 +1,19 @@
+
+
+import DrKatherineNasa from './katherine-johnson-nasa-sw-engineer-in-1966-.jpg';
+// import './MyNavBar.css';
+
+function MyNavBar() {
+    return (
+        <div className="MyNavBar">
+
+                <img src={DrKatherineNasa} className="" alt="logo" />
+                <p className="text-6xl">
+                 Women - the original software engineers.
+                </p>
+
+        </div>
+    );
+}
+
+export default MyNavBar;
